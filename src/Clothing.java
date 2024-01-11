5302f50b5035e5594eea8a99048a2ecedec2e0b1
Senuja Jayasekara <senujajayaekara@gmail.com>
2024-01-11 17:10:32 +0530

    puca

--- src/Clothing.java
+++ src/Clothing.java
@@ -1,2 +1,31 @@
 public class Clothing extends Product{
+
+    double size;
+    String colour;
+
+    Clothing(String productID,String productName,int availableItems,double price,String colour,double size){
+        super(productID,productName,availableItems,price);
+
+        this.size=size;
+        this.colour=colour;
+    }
+
+    public String getColour(){
+        return colour;
+    }
+
+    public  double getSize(){
+        return size;
+    }
+
+    public void setColour(String colour){
+        this.colour=colour;
+
+    }
+
+    public void setSize(double size){
+        this.size=size;
+    }
+
 }
+
--- src/Electronics.java
+++ src/Electronics.java
@@ -1,3 +1,33 @@
 public class Electronics extends Product
 {
+    private String brand;
+    private int warranty;
+
+    Electronics(String productID,String productName,int availableItems,double price,String brand,int warranty){
+        super(productID,productName,availableItems,price);
+
+
+        this.brand=brand;
+        this.warranty=warranty;
+
+    }
+
+
+    public String getBrand(){
+        return brand;
+    }
+
+    public  int getWarranty(){
+        return warranty;
+    }
+
+    public void setBrand(String brand){
+        this.brand=brand;
+
+    }
+
+    public void setWarranty(int warranty){
+        this.warranty=warranty;
+    }
+
 }
